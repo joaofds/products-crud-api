5535416dd4cf493302aab3e157ba7bca669b3feb
João Ferreira <jo4o.fds@gmail.com>
2021-08-16 21:53:27 -0300

    finalizado criacao de migrates, models e controllers

---
+++ database/migrations/2021_08_16_234438_create_addresses_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->integer('customer_id');
+            $table->string('street');
+            $table->string('number');
+            $table->string('zipcode');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->string('description')->nullable();
+            $table->boolean('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
